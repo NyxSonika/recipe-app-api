884ec7bcf87ea7a15029081f4ed470497c2df778
NyxSonika <soniiica99@gmail.com>
2023-10-02 12:02:07 +0545

    lets see

---
+++ app/recipe/serializers.py
@@ -0,0 +1,63 @@
+"""
+    Serializer gor recipe APIS
+"""
+from rest_framework import serializers
+
+from core.models import Recipe
+# Tag, Ingredient,
+
+
+# class TagSerializer(serializers.ModelSerializer):
+#     """Serializer for tag object"""
+
+#     class Meta:
+#         model = Tag
+#         fields = ('id', 'name')
+#         read_only_fields = ('id',)
+
+
+# class IngredientSerializer(serializers.ModelSerializer):
+#     """Serializer for an ingredient object"""
+
+#     class Meta:
+#         model = Ingredient
+#         fields = ('id', 'name')
+#         read_only_fields = ('id',)
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    """Serialize a recipe"""
+    # ingredients = serializers.PrimaryKeyRelatedField(
+    #     many=True,
+    #     queryset=Ingredient.objects.all()
+    # )
+    # tags = serializers.PrimaryKeyRelatedField(
+    #     many=True,
+    #     queryset=Tag.objects.all()
+    # )
+
+    class Meta:
+        model = Recipe
+        fields = (
+            'id', 'title', 'time_minutes',
+            'price', 'link'
+        )
+        read_only_fields = ('id',)
+
+
+# class RecipeDetailSerializer(RecipeSerializer):
+    """Serialize a recipe detail"""
+
+    # class Meta(RecipeSerializer.Meta):
+        # fields = RecipeSerializer.Meta.fields + ['description']
+#     ingredients = IngredientSerializer(many=True, read_only=True)
+#     tags = TagSerializer(many=True, read_only=True)
+
+#
+# class RecipeImageSerializer(serializers.ModelSerializer):
+#     """Serializer for uploading images to recipes"""
+
+#     class Meta:
+#         model = Recipe
+#         fields = ('id', 'image')
+#         read_only_fields = ('id',)
---
+++ app/recipe/views.py
@@ -0,0 +1,41 @@
+"""
+Views for recpie APIs
+"""
+
+from rest_framework import viewsets
+# , mixins, status
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+
+from core.models import Recipe
+# Tag, Ingredient,
+from recipe import serializers
+
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    """Manage recipes in the database"""
+    serializer_class = serializers.RecipeSerializer
+    queryset = Recipe.objects.all()
+    authentication_classes = (TokenAuthentication)
+    permission_classes = (IsAuthenticated)
+
+    # def _params_to_ints(self, qs):
+    #     """Convert a list of string IDs to a list of integers"""
+    #     return [int(str_id) for str_id in qs.split(',')]
+    def get_queryset(self):
+        """Retrieve the recipes for the authenticated user"""
+        return self.queryset.filter(user=self.request.user).order_by("-id")
+
+    def get_serializer_class(self):
+        """Return appropriate serializer class"""
+        if self.action == 'list':
+            return serializers.RecipeSerializer
+        # elif self.action == 'upload_image':
+        #     return serializers.RecipeImageSerializer
+
+        return self.serializer_class
+
+    def perform_create(self, serializer):
+        """Create a new recipe"""
+        serializer.save(user=self.request.user)
